279ce71c485ce26e0f5b6cdc860d45fbcdddadcf
JoshuaIsikwe <IsikweJoshua@gmail.com>
2022-05-25 01:12:04 +0200

    New images

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import "./style.css"
+import investment from  "../images/Investment.jpg"
+import consult from  "../images/consult.jpg"
+import trade from  "../images/trade.jpg"
 
 const Services = () => {
   return (
@@ -11,21 +14,25 @@ const Services = () => {
         
         <div className="service-section">
           <div className="service-card">
-            <div className='services-box-name'>Investment</div>
-            <div className='services-box-icon'><i class="fa-solid fa-user"></i></div>
+            <div className='services-box-name'><h2>Investment</h2></div>
+            <div className='services-box-icon'><img src={investment} alt="" /></div>
           </div>
           <div className="service-card">
-            <div className='services-box-icon'><i class="fa-solid fa-user"></i></div>
-            <div className='services-box-name'>Consulting & Advisory Services</div>
+            <div className='services-box-icon'><img src={consult} alt="" /></div>
+            <div className='services-box-name'><h2>Consulting & Advisory Services</h2></div>
           </div>
           <div className="service-card">
-            <div className='services-box-name'>Trade</div>
-            <div className='services-box-icon'><i class="fa-solid fa-user"></i></div>
+            <div className='services-box-name'><h2>Trade</h2></div>
+            <div className='services-box-icon'><img src={trade} alt="" /></div>
           </div>
         </div>
-    
+        
         <div className="industries">
-            <p>Clemmeridian Group so far has been able to work in various industries including</p>
+            <h3>Clemmeridian Group so far has been able to work in various industries including</h3>
+            <ul></ul>
+            <ul></ul>
+            <ul></ul>
+            <ul></ul>
             <ul></ul>
         </div>
     </div>
