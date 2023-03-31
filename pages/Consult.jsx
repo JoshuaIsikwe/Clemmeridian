74c85cfb4f91d08687fc1edecff35674034e9400
JoshuaIsikwe <IsikweJoshua@gmail.com>
2023-03-31 14:11:38 +0200

    Consults

--- pages/Consult.jsx
+++ pages/Consult.jsx
@@ -7,19 +7,19 @@ const Consult = () => {
     <div>
     <div className='header relative w-full text-center mb-16'>
         <Image src='/images/Consult1.jpg' width='1360' height='500' priority alt='/Financial Consulting'></Image>
-      <div className='division-title absolute uppercase font-bold text-justify bottom-1/3 left-4 text-xl md:text-4xl lg:text-6xl'>Clemmeridian Consult</div>
+      <div className='division-title absolute uppercase font-bold text-justify bottom-1/3 left-4 text-xl md:text-4xl lg:text-6xl'>Clemmeridian Consults</div>
       <div className='division-line absolute w-5/6 bottom-1/4 left-4 h-2 bg-gradient-to-r from-red-500 to-blue-600'></div>
     </div>
     <div className='foundation-body'>
         <div>
             
             <div className='line-1'></div>
-         
+          
             <div className='section-1 flex flex-col items-center'>
               <div className='left-section flex-1 w-4/6' >
               <p className='font-semibold  text-red-500'>Strategic consulting & advisory services, business development and training</p>
               
-                <h5>Clemmeridian Consult is a subsidiary of the group and specializes in tax and financial management consulting, business development and training.</h5>
+                <h5>Clemmeridian Consults is a subsidiary of the group and specializes in tax and financial management consulting, business development and training.</h5>
                 <p>Provides strategic advisory services to State Internal Revenue Services and tax authorities on tax revenue growth.
                 The services rendered by Clemmeridian Consults, being a tax and financial advisory entity include: 
                 </p>
